0aedad970d2e4cedd874769f8b92c6db75d88c8c
Matias <matias.tieranta@edu.turkuamk.fi>
2021-01-11 15:25:57 +0200

    T2 number of lists

---
+++ T2.py
@@ -0,0 +1,14 @@
+# empty list
+lst = []
+
+# lets set numbers of elements to list 10
+n = 10
+
+# aks to user fill 10 numbers
+for i in range(0, n):
+    ele = int(input("Give 10 numbers please :"))
+
+    # add users numbers to our empty list
+    lst.append(ele)  # adding the element
+
+print(lst)
