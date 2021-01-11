40c531bea4a38d41f266c6a936c05bb28eda8744
Matias <matias.tieranta@edu.turkuamk.fi>
2021-01-11 17:09:23 +0200

    t1 -> t5

--- T2.py
+++ T2.py
@@ -3,6 +3,7 @@ import random
 # empty list
 
 lst_numbers = []
+lst_numbers_negative = []
 
 # lets set numbers of elements to list 10
 n = 10
@@ -10,6 +11,10 @@ n = 10
 # aks to user fill 10 numbers
 for i in range(0, n):
     ele = int(input("Give 10 numbers please :"))
+    if ele == 0:
+        break
+    elif ele < 0:
+        lst_numbers_negative.append(ele)
 
     # add users numbers to our empty list
     lst_numbers.append(ele)  # adding the element
@@ -26,4 +31,6 @@ randomlist = random.sample(range(0, 1000), 10)
 lst_numbers.sort()
 randomlist.sort()
 lst_strings.sort()
-print(lst_numbers, lst_strings, randomlist)
+
+print(lst_numbers, lst_strings, randomlist, lst_numbers_negative)
+
