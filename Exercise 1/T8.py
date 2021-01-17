d8e7e746b0dc92c12294711076e94607faa90956
Matias <matias.tieranta@edu.turkuamk.fi>
2021-01-17 16:20:16 +0200

    Workings

--- Exercise 1/T8.py
+++ Exercise 1/T8.py
@@ -1,30 +1,51 @@
+# File name: Task 9 of exercise 1
+# Author: Matias Tieranta
+# Description: Code a simple (and textual) implementation of Rock-Paper-Scissors game. Best of 3 games wins.
 import random
 
-print("welcome")
-chars = "RPS"
+player_points = 0
+computer_points = 0
 
 
-def rps(player1, player2):
-    if player1 == "R" and player2 == "P":
-        return "computer won!!"
-    elif player1 == "P" and player2 == "R":
-        return "player1 won!!"
-    elif player1 == "P" and player2 == "S":
-        return "computer won!!"
-    elif player1 == "S" and player2 == "P":
-        return "player1 won!!"
-    elif player1 == "R" and player2 == "S":
-        return "player1 won!!"
-    elif player1 == "S" and player2 == "P":
-        return "computer won!!"
-    elif player1 not in chars:
-        return "wrong input"
-    elif player1 == player2:
+def rps():
+    global player_points
+    global computer_points
+    player_turn = input("Rock, Paper, Scissors ! ")
+
+    computer_turn = random.randint(1, 3)
+    if computer_turn == 1:
+        computer_turn = "Rock"
+    elif computer_turn == 2:
+        computer_turn = "Paper"
+    elif computer_turn == 3:
+        computer_turn = "Scissors"
+
+    print("player_turn", player_turn)
+    print("computer_turn", computer_turn)
+
+    if player_turn == "Rock" and computer_turn == "Paper":
+        computer_points += 1
+        return "computer_turn won!!"
+    elif player_turn == "Paper" and computer_turn == "Rock":
+        player_points += 1
+        return "player_turn won!!"
+    elif player_turn == "Paper" and computer_turn == "Scissors":
+        computer_points += 1
+        return "computer_turn won!!"
+    elif player_turn == "Scissors" and computer_turn == "Paper":
+        player_points += 1
+        return "player_turn won!!"
+    elif player_turn == "Rock" and computer_turn == "Scissors":
+        player_points += 1
+        return "player_turn won!!"
+    elif player_turn == "Scissors" and computer_turn == "Paper":
+        computer_points += 1
+        return "computer_turn won!!"
+    elif player_turn == computer_turn:
         return "tie"
 
 
-run = True
-while run:
-    player1 = input("R for rock ,P for Paper,S for scissors : \n")
-    player2 = random.choice(chars)
-    print(rps(player1, player2))
+while (player_points < 2) and (computer_points < 2):
+    print(rps())
+    print("Player points :", player_points)
+    print("Computer points :", computer_points)
