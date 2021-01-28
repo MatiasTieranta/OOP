698956c56f4bd5b610d0240eb5b6c81fcd6dd4fe
Matias <matias.tieranta@edu.turkuamk.fi>
2021-01-28 21:31:07 +0200

    Exersice2

--- Exercise 3/Exersice2.py
+++ Exercise 3/Exersice2.py
@@ -3,16 +3,13 @@
 # Description: Flipping coin game
 import random
 
-toss = random.randint(0, 3)
-currency = range(0, 4)
-
 
 class Coin:
 
     # The__init__ method initializes the sideup data attribute with 'Heads'
     def __init__(self):
-        self.get_currency = 'Euro'
         self.__sideup = 'Heads'
+        self.__currency = 'Euro'
 
     # The toss method generate a random number
     # int the range 0 to 3. If the number
@@ -22,16 +19,30 @@ class Coin:
     # if 3 sideup is set to 'you missed the table'
 
     def toss(self):
-        if toss == 0:
+        toss_result = random.randint(0, 3)
+        if toss_result == 0:
             self.__sideup = 'Heads'
-        elif toss == 1:
+        elif toss_result == 1:
             self.__sideup = 'tails'
 
-        elif toss == 2:
+        elif toss_result == 2:
             self.__sideup = 'Coin land on Upright!'
 
-        elif toss == 3:
+        elif toss_result == 3:
             self.__sideup = 'Oh no you missed the table'
+
+    def toss_currency(self):
+        toss_result = random.randint(0, 4)
+        if toss_result == 0:
+            self.__currency = 'Euro'
+        elif toss_result == 1:
+            self.__currency = 'Pound'
+        elif toss_result == 2:
+            self.__currency = 'Dollar'
+        elif toss_result == 3:
+            self.__currency = 'Ruble'
+        elif toss_result == 4:
+            self.__currency = 'Yen'
 
     # The get_sideup method return the value referenced by sideup
 
@@ -39,17 +50,7 @@ class Coin:
         return self.__sideup
 
     def get_currency(self):
-        if currency == 0:
-            self.get_currency = 'Euro'
-        elif currency == 1:
-            self.get_currency = 'Pound'
-        elif currency == 2:
-            self.get_currency = 'Dollar'
-        elif currency == 3:
-            self.get_currency = 'Rubble'
-        elif currency == 4:
-            self.get_currency = 'Yen'
-
+        return self.__currency
 
 
 # The main function
@@ -58,11 +59,12 @@ class Coin:
 def main():
     # Crete an object from Coin class.
     my_coin = Coin()
+    my_coin.toss_currency()
     # Currency
-    print("Currency is :", my_coin.get_currency)
+    print("Currency is :", my_coin.get_currency())
 
     # Display the side of the coin that is facing up.
-    print('This side is up and currently we are on this currency: ', my_coin.get_sideup())
+    print('This side is up : ', my_coin.get_sideup())
 
     # Toss the coin
     print("Im tossing the coin...")
