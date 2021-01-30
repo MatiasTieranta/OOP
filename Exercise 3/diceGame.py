6a9b7c2431bd8f03d2f8b3f4165f3a8287120149
Matias <matias.tieranta@edu.turkuamk.fi>
2021-01-30 19:31:59 +0200

    Extra feature and typo chekking

--- Exercise 3/diceGame.py
+++ Exercise 3/diceGame.py
@@ -1,25 +1,29 @@
-# File: CoinTossingGame
+# File:diceTossingGame
 # Author: Matias
-# Description: Flipping coin game
+# Description: diceGame
+
 import random
 
 
 class Dice:
 
-    # The__init__ method initializes the sideup data attribute with 'Heads'
+    # The__init__ method initializes the sideup data attribute with 'Number one and Orange'
     def __init__(self):
         self.__sideup = '1'
         self.__colour = 'Orange'
 
     # The toss method generate a random number
-    # int the range 0 to 3. If the number
-    # 0, then sideup is set to 'Heads'
-    # if 1 sideup is set to 'Tails'
-    # if 2 sideup is set to 'Upright'
-    # if 3 sideup is set to 'you missed the table'
+    # int the range 1 to 6. If the number
+    # 1, then sideup is set to 'Orange and number One'
+    # if 2 sideup is set to 'Black and number Two'
+    # if 3 sideup is set to 'White and number Three'
+    # if 4 sideup is set to 'Pink and number Four'
+    # if 5 sideup is set to 'Turquoise and number five'
+    # if 6 sideup is set to 'Red and number Six'
+    # As a extra feature we can also lose our dice and its set to 'zero and none'
 
     def toss_number(self):
-        toss_result = random.randint(1, 6)
+        toss_result = random.randint(0, 6)
         self.__sideup = toss_result
         if toss_result == 1:
             self.__colour = 'Orange'
@@ -33,7 +37,9 @@ class Dice:
             self.__colour = 'Turquoise'
         elif toss_result == 6:
             self.__colour = 'Red'
-
+        elif toss_result == 0:
+            print('Where did that dice even go we need to start again...')
+            self.__colour = None
     # The get_sideup method return the value referenced by sideup
 
     def get_sideup(self):
@@ -47,17 +53,18 @@ class Dice:
 
 
 def main():
-    # Crete an object from Coin class.
+
+    # Crete an object from Dice class.
     my_dice = Dice()
 
-    # Display the side of the coin that is facing up.
+    # Display the side of the dice that is facing up.
     print('This side is up : ', my_dice.get_sideup(), my_dice.get_colour())
 
-    # Toss the coin
-    print("Im tossing the coin...")
+    # Toss the dice
+    print("Im tossing the dice...")
     my_dice.toss_number()
 
-    # Display the side of the coin that is facing up.
+    # Display the side of the dice that is facing up.
     print('This side is up: ', my_dice.get_sideup(), my_dice.get_colour())
 
 
