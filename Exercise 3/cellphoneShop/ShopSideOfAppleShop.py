ec0e537326891bd87fb00562b66f398501454792
Matias <matias.tieranta@edu.turkuamk.fi>
2021-01-31 17:27:30 +0200

    Works as intended

--- Exercise 3/cellphoneShop/ShopSideOfAppleShop.py
+++ Exercise 3/cellphoneShop/ShopSideOfAppleShop.py
@@ -7,23 +7,23 @@ class Cellphone:
 
     # The __init__ method initializes retail prize, model and manufacturer
 
-    def __init__(self, __retailprice, owner1, owner2):
-        self.__retailprice = __retailprice
-        self.__owner1 = owner1
-        self.__owner2 = owner2
+    def __init__(self, retailprice, manufacturer, model):
+        self.__retailprice = retailprice
+        self.manufacturer = manufacturer
+        self.model = model
 
     # The get_balance method returns retail price.
     def get_balance(self):
         return self.__retailprice
 
     # The get_owner1 method returns the manufacturer of phone
-    def get_owner1(self):
-        return self.__owner1
-
+    def get_manufacturer(self):
+        return self.manufacturer
     # The get_owner2 method returns the model of phone
-    def get_owner2(self):
-        return self.__owner2
+
+    def get_model(self):
+        return self.model
 
     # The __str__ method returns a string indicating the objects state
     def __str__(self):
-        return f'''manufacturer: {self.__owner1} model: {self.__owner2} retailprice: {self.__retailprice} '''
+        return f'''manufacturer: {self.manufacturer} model: {self.model} retailprice: {self.__retailprice} '''
--- Exercise 3/cellphoneShop/UserSideOfAppleShop.py
+++ Exercise 3/cellphoneShop/UserSideOfAppleShop.py
@@ -7,19 +7,19 @@ import ShopSideOfAppleShop
 
 def main():
     # Get the manufacturer of the phone
-    owner1 = input('Enter the manufacturer :')
+    manufacturer = input('Enter the manufacturer :')
 
-    # Get the model of the fone
-    owner2 = input('Enter the owner: ')
+    # Get the model of the phone
+    model = input('Enter the model: ')
 
     # Get the retailprice of phone
     retail_price = float(input('enter the retail price: '))
 
     # Create a BankAccount object.
-    savings = ShopSideOfAppleShop.Cellphone(retail_price, owner1, owner2)
+    phoneSpecs = ShopSideOfAppleShop.Cellphone(retail_price, manufacturer, model)
 
     # Display the balance
-    print(savings)
+    print(phoneSpecs)
 
 
 main()
