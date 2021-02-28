3444d300adb038923abcbce0bb9f62bb3c5f982c
Matias <matias.tieranta@edu.turkuamk.fi>
2021-02-28 16:28:51 +0200

    Mammals done with subclasses done with styleguide

--- Exersixe6/Task5/Task5.py
+++ Exersixe6/Task5/Task5.py
@@ -1,3 +1,8 @@
+# File: mammals.py
+# Author: Matias Tieranta
+# Description: Example how does python inherits work with two sub classes
+
+# Lets create base class
 class Mammal:
 
     # The __init__ method initializes retail prize, model and manufacturer
@@ -9,6 +14,7 @@ class Mammal:
         self.__weight = weight
         self.__dimensions = dimensions
 
+    # Defines get and set to be used to sub classes
     def get_id(self):
         return self.__id
 
@@ -48,6 +54,7 @@ class Mammal:
         \ndimensions: {self.__dimensions}'''
 
 
+# Lets create domestic class from mammals
 class domestic_animal(Mammal):
 
     def __init__(self, id, name, size, weight, dimensions, domestic, diet, noise):
@@ -56,12 +63,16 @@ class domestic_animal(Mammal):
         self.__diet = diet
         self.__noise = noise
 
+    # The __str__ method returns a string indicating the objects state
     def __str__(self):
         return 'id of mammal is' + self.get_id() + '\nname of mammals is ' + self.get_name() + \
                '\nsize of animal' + self.get_size() + '\nWeight of mammal is' + self.get_weight() + \
                '\nDimension of mammal is' + \
                self.get_dimensions() + '\ndiet of mammals is' + self.__diet + \
                '\nNoise of mammals is' + self.__noise + '\nAnimal is:' + self.__isDomestic
+
+
+# Lets create wild animals class from mammals
 
 class wild_animal(Mammal):
     def __init__(self, id, name, size, weight, dimensions, wild, diet, noise):
@@ -70,6 +81,7 @@ class wild_animal(Mammal):
         self.__diet = diet
         self.__noise = noise
 
+    # The __str__ method returns a string indicating the objects state
     def __str__(self):
         return 'id of mammal is' + self.get_id() + '\nname of mammals is ' + self.get_name() + \
                '\nsize of animal' + self.get_size() + '\nWeight of mammal is' + self.get_weight() + \
@@ -78,6 +90,7 @@ class wild_animal(Mammal):
                '\nNoise of mammals is' + self.__noise + '\nAnimal is:' + self.__isWild
 
 
+# Give values to inherit classes
 bat = wild_animal(' 1', 'Bat', ' small', ' 200g', ' 10', 'Wild animal', ' insects', ' Na-nana-nana-na')
 whale = wild_animal(' 2', 'Whale', 'huge', ' 1000kg', ' 10m', ' Wild animal', ' makril', ' Tick')
 sealion = wild_animal(' 3', 'SeaLion', ' big', ' 300kg', ' 300cm', ' Wild animal', ' sardines vocals', 'threat vocals')
@@ -86,6 +99,8 @@ cat = domestic_animal(' 4', 'cat', ' normal', ' 4kg', ' 20cm', 'Domestic animal'
 dog = domestic_animal(' 5', 'dog', ' normal', ' 4kg', ' 20cm', ' Domestic animal', ' steak', 'barking')
 bunny = domestic_animal(' 6', 'bunny', ' small', ' 2kg', ' 15cm', ' Domestic animal', ' carrots', 'sniff')
 
+
+# Lets print out all classes
 print('Here are some wild animals')
 print(bat)
 print()
