2e4f0a787a2c93e1d171c1f1cf853e9eb4bff192
Matias <matias.tieranta@edu.turkuamk.fi>
2021-02-28 16:38:38 +0200

    Students and techers done with styleguide

--- Exersixe6/OOPcourse/course_of_oop.py
+++ Exersixe6/OOPcourse/course_of_oop.py
@@ -1,3 +1,9 @@
+# File: course_of_oop.py
+# Author: Matias Tieranta
+# Description: Example how does python inherits work with two sub classes
+
+# Lets create a base class
+# With some common of both
 class Course:
     def __init__(self, id, name, school):
         self.__id = id
@@ -30,6 +36,7 @@ class Course:
             \nschool: {self.__school}'''
 
 
+# Lets create subclass called Students
 class Students(Course):
 
     def __init__(self, id, name, school, isStudent, tasksDone, grade):
@@ -38,39 +45,51 @@ class Students(Course):
         self.__tasksDone = tasksDone
         self.__grade = grade
 
+        # The __str__ method returns a string indicating the objects state
+
     def __str__(self):
         return 'id' + self.get_id() + '\nname' + self.get_name() + \
                '\nSchool' + self.get_school() + '\nPerson is' + \
                self.__isStudent + '\nTasks done' + self.__tasksDone + '\nGrade' + self.__grade
 
 
+# Lets create subclass called teachers
 class Teachers(Course):
 
-    def __init__(self, id, name, school, isTeacher, cofeeBreaks, salary):
+    def __init__(self, id, name, school, is_teacher, cofee_breaks, salary):
         super().__init__(id, name, school)
-        self.__isTeacher = isTeacher
-        self.__cofeeBreaks = cofeeBreaks
+        self.__isTeacher = is_teacher
+        self.__cofeeBreaks = cofee_breaks
         self.__salary = salary
+
+        # The __str__ method returns a string indicating the objects state
 
     def __str__(self):
         return 'id' + self.get_id() + '\nname' + self.get_name() + \
                '\nSchool' + self.get_school() + '\nPerson is' + \
-               self.__isTeacher + '\nHow many cofee breaks he/she had' + self.__cofeeBreaks+ \
+               self.__isTeacher + '\nHow many cofee breaks he/she had' + self.__cofeeBreaks + \
                '\nSalary' + self.__salary
 
+
+# Gives values to students
 
 markus = Students(' 1', ' Markus', ' Turun AMK', ' a student', ' 11', ' 5')
 jonna = Students('2', 'Jonna', 'Turun AMK', 'is a student', '5', '1')
 
+# Gives value to teachers
+
 ossi = Teachers(' 11', ' Ossi', ' Turun AMK', ' a teacher', ' 3', ' 2400e')
 jaana = Teachers(' 120', ' Jaana', ' Turun AMK', ' a teacher', ' 11', ' 3400e')
 
+# Prints out two of student class inherit from course
 
 print('Here are our students :')
 print(markus)
 print()
 print(jonna)
 print()
+
+# Prints out two of teachers class inherit from cource
 
 print('Here are our teachers :')
 print(ossi)
